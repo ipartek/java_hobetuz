cec9989ca508926e6d8d8f9a342f02809e4b91b3
anderuraga <ander.uraga@gmail.com>
2019-04-04 10:29:48 +0200

    conversor euros

--- src/com/ipartek/formacion/proyecto/operadores/artimeticos/ConversorEuros.java
+++ src/com/ipartek/formacion/proyecto/operadores/artimeticos/ConversorEuros.java
@@ -1,10 +1,28 @@
 package com.ipartek.formacion.proyecto.operadores.artimeticos;
 
+import java.util.Scanner;
+
 public class ConversorEuros {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		final float VALOR_DOLAR = (float)1.12;	
+		
+		System.out.println("1€ = " + VALOR_DOLAR + " $");
+
+		float euros = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("¿Cuantos euros tienes? ");
+		
+		euros = sc.nextFloat();
+		
+		System.out.println("En dolares = " + (euros * VALOR_DOLAR) );
+		
+		sc.close();
+		
 	}
 
 }
